f3e7c3576a723e237f1a1f5f2f7687ad670ef4cc
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2018-08-25 21:26:18 +0200

    Import base structure

---
+++ src/index.js
@@ -0,0 +1,63 @@
+import VueRouter from 'vue-router';
+import Vue from 'vue';
+
+import Notifications from 'vue-notification';
+import VueClipboards from 'vue-clipboards';
+import Raven from 'raven-js';
+import RavenVue from 'raven-js/plugins/vue';
+
+import App from './components/app.vue';
+import routes from './modules/routes';
+import SharedState from './modules/shared-state';
+// Uncomment when a Vuex is required
+// @see https://vuex.vuejs.org/
+// import store from './store';
+// Uncomment when an event bus is required
+// @see https://vuejs.org/v2/style-guide/#Non-flux-state-management-use-with-caution  
+// import EventHub from './modules/event-hub';
+import Styles from './styles';
+import Config from './config';
+
+Vue.config.productionTip = false;
+
+Vue.use(VueRouter);
+Vue.use(Notifications);
+Vue.use(VueClipboards);
+
+if (SharedState.state.productionMode) {
+  const dsn = Config.raven.dsn;
+  Raven.config(dsn)
+  .addPlugin(RavenVue, Vue)
+  .install();
+}
+
+const router = new VueRouter({
+  routes,
+  scrollBehavior() {
+    return { x: 0, y: 0 };
+  },
+});
+
+router.beforeEach((to, from, next) => {
+  if (typeof from.query.peek !== 'undefined'
+  && typeof to.query.peek === 'undefined'
+  ) {
+    next({ name: to.name, query: from.query });
+    return;
+  }
+
+  next();
+});
+
+// eslint-disable-next-line
+const app = new Vue({
+  el: '#app',
+  router,
+  // store,
+  style: Styles.styles,
+  components: {
+    App,
+  },
+});
+
+window.app = app;
---
+++ src/styles/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import {
+// } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+import styles from './global.css';
+
+// See https://fontawesome.com/how-to-use/on-the-web/using-with/vuejs
+// library.add();
+
+// Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+export default {
+  Styles: styles,
+  // components: {
+  //   'font-awesome-icon': FontAwesomeIcon,
+  // },
+};
