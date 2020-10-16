1e67dd91c42f45a34427f69a97e60cd930de0839
Thierry Marianne <thierry@marianne.io>
2020-10-16 16:10:47 +0200

    Lint project

--- src/mixins/with-uuid.js
+++ src/mixins/with-uuid.js
@@ -2,10 +2,10 @@ import uuidv5 from 'uuid/v5';
 
 const WithUuid = {
   methods: {
-    generateUuid: function (name, namespace) {
+    generateUuid: function(name, namespace) {
       return uuidv5(`${name}`, namespace);
-    },
-  },
+    }
+  }
 };
 
 export default WithUuid;
