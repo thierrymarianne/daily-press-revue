f4a6b87f57962ebfe1f291537ef8c1681417ae3b
Thierry Marianne <thierry@marianne.io>
2020-12-12 21:46:04 +0100

    Align introduction paragraph vertically

--- src/components/intro/intro.vue
+++ src/components/intro/intro.vue
@@ -3,11 +3,13 @@
     <div class="intro__logo-container">
       <img class="intro__logo" :src="logo" alt="Revue de Presse logo"/>
     </div>
-    <p class="intro__description">Revue 2 presse est un <a
-        class="intro__project"
-        :href="currentRoute + '#project'">projet citoyen indépendant
-      </a> qui s'adresse aux journalistes et à toute personne qui s'intéresse à l'actualité et à l'influence des médias sur l'opinion.
-    </p>
+    <div class="intro__description-container">
+      <p class="intro__description">Revue 2 presse est un <a
+          class="intro__project"
+          :href="currentRoute + '#project'">projet citoyen indépendant
+        </a> qui s'adresse aux journalistes et à toute personne qui s'intéresse à l'actualité et à l'influence des médias sur l'opinion.
+      </p>
+    </div>
   </div>
 </template>
 
