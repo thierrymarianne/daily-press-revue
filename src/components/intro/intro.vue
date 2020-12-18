ced52c60bbd1a7d41a5be23d622c85505d4ed191
Thierry Marianne <thierrymarianne@users.noreply.github.com>
2020-12-18 13:56:53 +0100

    Refresh overall style (#80)

--- src/components/intro/intro.vue
+++ src/components/intro/intro.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="intro">
-    <div class="intro__logo-container">
-      <img class="intro__logo" :src="logo" alt="Revue de Presse logo"/>
-    </div>
-    <div class="intro__description-container">
-      <p class="intro__description">Revue 2 presse est un <a
-          class="intro__project"
-          :href="currentRoute + '#project'">projet citoyen indépendant
-        </a> qui s'adresse aux journalistes et à toute personne qui s'intéresse à l'actualité et à l'influence des médias sur l'opinion.
-      </p>
+    <div class="intro__container">
+      <div class="intro__logo-container">
+        <a href="/">
+          <img class="intro__logo" :src="logo" alt="Revue de Presse logo"/>
+        </a>
+      </div>
+      <div class="intro__description-container">
+        <p class="intro__description">Revue 2 presse est un <a
+            class="intro__project"
+            :href="currentRoute + '#project'">projet citoyen indépendant
+          </a> qui s'adresse aux journalistes et à toute personne s'intéressant à l'actualité et à l'influence des médias sur l'opinion.
+        </p>
+      </div>
     </div>
   </div>
 </template>
